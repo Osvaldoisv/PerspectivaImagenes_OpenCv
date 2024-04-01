364d1918da1a08a1c7a692fc7e35b164a126e5e6
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-03-31 21:39:31 -0300

    funcionando

--- perspective_transformation_points.py
+++ perspective_transformation_points.py
@@ -8,6 +8,13 @@ cv2.circle(imagen, (513,77), 7, (0,255,0), 2)
 cv2.circle(imagen, (113,358), 7, (0,0,255), 2)
 cv2.circle(imagen, (542,366), 7, (255,255,0), 2)
 
+pts1 = np.float32([[84, 69], [513, 77], [113, 358], [542, 366]]) # Ojo con el orden de vertices
+pts2 = np.float32([[0,0], [480,0], [0,300], [480,300]]) # Ojo con el orden de vertices
+
+M = cv2.getPerspectiveTransform(pts1, pts2)
+dst = cv2.warpPerspective(imagen,  M, (480,300))
+
 cv2.imshow('Imagen', imagen)
+cv2.imshow('dst', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
